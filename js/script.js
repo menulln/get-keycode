1b0fb2be78f58ed4099370d53a554975673ce776
menulln <39300523+menulln@users.noreply.github.com>
2022-11-06 01:13:24 +0100

    Add getKeyCode function

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ const keyCode = document.querySelector('.keycode');
 const keyName = document.querySelector('.keyname');
 
 window.addEventListener('keydown', getKeyCode);
+
+function getKeyCode(e) {
+    keyCode.textContent = e.keyCode;
+    keyName.textContent = e.code;
+}

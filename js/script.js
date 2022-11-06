ad790ae08c1dfbdb8969e77f77ad009f84d098f1
menulln <39300523+menulln@users.noreply.github.com>
2022-11-06 01:17:32 +0100

    Get header-button and attach EventListener

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 const keyCode = document.querySelector('.keycode');
 const keyName = document.querySelector('.keyname');
+const headerButton = document.querySelector('.header-button');
 
 window.addEventListener('keydown', getKeyCode);
+headerButton.addEventListener('click', switchColorTheme);
 
 function getKeyCode(e) {
     keyCode.textContent = e.keyCode;
